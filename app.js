394f5b129a79b6136f550291196d9b7c6672d539
Jonas Johansson <jonas.johansson@chalmers.se>
2021-02-12 14:37:59 +0100

    Added instancetype Tryck to magicbutton

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.get("/bookmarklet.js", (req, res) => {
   let jsString = `
   if (location.host === "libris.kb.se") {
     if (/^\\/katalogisering\\/[\\w\\d]{15}/.test(location.pathname)) {
-      if ((document.getElementById("formPath-mainEntity.@type").childNodes[6].textContent.trim().indexOf("Instans") > -1) && 
+      if (((document.getElementById("formPath-mainEntity.@type").childNodes[6].textContent.trim().indexOf("Tryck") > -1) || 
+           (document.getElementById("formPath-mainEntity.@type").childNodes[6].textContent.trim().indexOf("Instans") > -1)) && 
         (document.getElementsByClassName("CreateItem")[0].textContent.trim().indexOf("Visa bestånd") > -1)) {
         let librisurl = \`https://\${location.host}\${location.pathname.replace(
           "/katalogisering",
